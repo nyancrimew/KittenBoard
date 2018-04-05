334bc462321f8f545ae78e4600e42c26cd3a49d8
Joey <joey@lineageos.org>
2018-04-05 15:21:52 +0200

    LatinIME: support for incognito mode
    Don't learn suggestions when input specifies the
    IME_FLAG_NO_PERSONALIZED_LEARNING flag
    
    Change-Id: I224c36deda560657996cffa2922392a64e45b53f
    Signed-off-by: Joey <joey@lineageos.org>

--- java/src/com/android/inputmethod/latin/settings/SettingsValues.java
+++ java/src/com/android/inputmethod/latin/settings/SettingsValues.java
@@ -184,7 +184,8 @@ public class SettingsValues {
                 && prefs.getBoolean(Settings.PREF_GESTURE_FLOATING_PREVIEW_TEXT, true);
         mAutoCorrectionEnabledPerUserSettings = mAutoCorrectEnabled
                 && !mInputAttributes.mInputTypeNoAutoCorrect;
-        mSuggestionsEnabledPerUserSettings = readSuggestionsEnabled(prefs);
+        mSuggestionsEnabledPerUserSettings = !mInputAttributes.mNoLearning &&
+                readSuggestionsEnabled(prefs);
         mIsInternal = Settings.isInternal(prefs);
         mHasCustomKeyPreviewAnimationParams = prefs.getBoolean(
                 DebugSettings.PREF_HAS_CUSTOM_KEY_PREVIEW_ANIMATION_PARAMS, false);
